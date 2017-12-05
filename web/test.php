d5e0687863e741e9bd7242e567250e58ffd9638b
tomoaki nagasaka <tomoaki_nagasaka@gyosei-system.co.jp>
2017-12-05 13:43:00 +0900

    更新

--- web/test.php
+++ web/test.php
@@ -87,9 +87,9 @@ $(function(){
 			  content: 'それでは、以下のリンクより属性登録をお願いします。'
 		  }).then(function() {
 			  return botui.message.add({
-			        delay: 1000,
+			        //delay: 1000,
 			        //content: '＞＞[属性登録](https://gyoseibot.herokuapp.com/attribute.php?user=w0e999b000test)'
-			        content: 'Go ahead, try [our product](https://example.com)'
+			        content: 'Go ahead, try [our product](https://example.com)^'
 			  });
 		  }).then(init);
 	  }
