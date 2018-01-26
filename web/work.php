021c49ab3e053693355ac90f6855eb1be8451c90
tomoaki nagasaka <tomoaki_nagasaka@gyosei-system.co.jp>
2018-01-26 11:33:34 +0900

    更新

--- web/work.php
+++ web/work.php
@@ -6,7 +6,6 @@
 <link href="css/common.css" rel="stylesheet" />
 <link href="css/bootstrap.css" rel="stylesheet" />
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.js"></script>
-<script src="js/bootstrap.js"></script>
 </head>
 <body>
 <div id="header"></div>
@@ -35,6 +34,7 @@
 	</div>
 </div>
 </body>
+<script src="js/bootstrap.js"></script>
 <script>
 $(function(){
 	$("#header").load("header.html");
