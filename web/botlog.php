9312456dda1f6c13dbf9f06ee6acef3340cea230
tomoaki nagasaka <tomoaki_nagasaka@gyosei-system.co.jp>
2018-01-26 11:53:16 +0900

    更新

--- web/botlog.php
+++ web/botlog.php
@@ -8,7 +8,6 @@
 <link href="css/bootstrap.css" rel="stylesheet" />
 <link href="css/jquery.bootgrid.css" rel="stylesheet" />
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.js"></script>
-<script src="js/bootstrap.js"></script>
 <script src="js/jquery.bootgrid.js"></script>
 </head>
 <body>
