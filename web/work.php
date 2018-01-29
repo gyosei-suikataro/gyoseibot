c782cf3031e538a5a7d1d891fbc71ec9c6209d45
tomoaki nagasaka <tomoaki_nagasaka@gyosei-system.co.jp>
2018-01-29 14:34:23 +0900

    更新

--- web/work.php
+++ web/work.php
@@ -9,22 +9,22 @@
 <div id="header"></div>
 <div class="container">
 	<div class="center-block">
-		<input type="button" class="btn btn-default" onclick="detailwin()" data-toggle="modal" value="モーダル表示" />
+		<input type="button" class="btn btn-default" onclick="detailwin()" data-toggle="modal"  data-target="#shosaiDialog" value="モーダル表示" />
 	</div>
 </div>
 <div class="modal" id="shosaiDialog"  tabindex="-1">
 	<div class="modal-dialog">
-		<div class="modal-content">
+		<div class="modal-content" style="width:740px;">
 			<div class="modal-header">
 				<button type="button" class="close" data-dismiss="modal">
 					<span aria-hidden="true">&times;</span>
 				</button>
 				<h4 class="modal-title" id="modal-label">詳細</h4>
 			</div>
-			<div class="modal-body">
+			<span class="modal-body">
 				<p style='display:inline;'>　　　　日時　</p>
 				<input id="date" type="text" readonly style="width: 600px;">
-			</div>
+			</span>
 			<div class="modal-footer">
 				<button type="button" class="btn btn-default" data-dismiss="modal">閉じる</button>
 			</div>
