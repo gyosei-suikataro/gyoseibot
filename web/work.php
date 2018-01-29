34734e75e2cae580139120629ad673c67c0c581c
tomoaki nagasaka <tomoaki_nagasaka@gyosei-system.co.jp>
2018-01-29 13:25:31 +0900

    更新

--- web/work.php
+++ web/work.php
@@ -8,7 +8,26 @@
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.js"></script>
 </head>
 <body>
-<div id="header"></div>
+	<nav class="navbar navbar-default navbar-static-top navbar-inverse">
+		<div class="container">
+			<ul class="nav navbar-nav">
+				<li class="active">
+				<a href="./index.php"><span class="glyphicon glyphicon-home"></span> Home</a></li>
+			</ul>
+			<ul class="nav navbar-nav navbar-right">
+				<li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><span class="glyphicon glyphicon-align-justify"></span> Menu<span class="caret"></span></a>
+					<ul class="dropdown-menu" role="menu">
+						<li><a href="./botlog.php"><span class="glyphicon glyphicon-list"></span> ログ参照</a></li>
+						<li><a href="./imagelog.php"><span class="glyphicon glyphicon-list"></span> 画像ログ参照</a></li>
+						<li><a href="./shisetsu.php"><span class="glyphicon glyphicon-list"></span> 施設情報</a></li>
+						<li><a href="./genre.php"><span class="glyphicon glyphicon-list"></span> 施設ジャンル</a></li>
+						<li><a href="./opinion.php"><span class="glyphicon glyphicon-list"></span> 市政へのご意見</a></li>
+						<li><a href="./test.php"><span class="glyphicon glyphicon-list"></span> ボットテスト</a></li>
+					</ul>
+				</li>
+			</ul>
+		</div>
+	</nav>
 <div class="container">
 	<div class="center-block">
 		<input type="button" class="btn btn-default" onclick="detailwin()" value="モーダル表示" />
@@ -35,9 +54,6 @@
 </div>
 </body>
 <script>
-$(function(){
-	$("#header").load("header.html");
-});
 
 function detailwin(){
 	$('#shosaiDialog').modal('show');
