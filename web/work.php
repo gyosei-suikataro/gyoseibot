5cc1647082e616403de23c3368869d4f6aac7e9a
tomoaki nagasaka <tomoaki_nagasaka@gyosei-system.co.jp>
2018-01-29 13:59:49 +0900

    更新

--- web/work.php
+++ web/work.php
@@ -3,8 +3,6 @@
 <head>
 <meta charset='utf-8'>
 <title>Menu</title>
-<link href="css/common.css" rel="stylesheet" />
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.js"></script>
 <script src="js/bootstrap.js"></script>
 </head>
 <body>
