c3211864c7f7b8bc7835ee5ad13d030da96d2470
tomoaki nagasaka <tomoaki_nagasaka@gyosei-system.co.jp>
2018-02-01 11:50:04 +0900

    更新

--- web/imagelog.php
+++ web/imagelog.php
@@ -105,12 +105,12 @@ if ($link) {
 	<input id="btn_modal" type="button" style="display:none" data-toggle="modal"  data-target="#image_Modal" value="モーダル表示" />
 </div>
 </div>
-<div class="modal" id="image_Modal" tabindex="-1" role="dialog">
+<div class="modal" id="image_Modal" tabindex="-1">
 	<div class="modal-dialog">
 		<div class="modal-content" id="dia_cont">
 			<div class="modal-body">
 				<p id="dia_kaku"></p>
-				<img  id="dia_image" class="aligncenter size-full wp-image-425"/>
+				<img  id="dia_image"/>
 			</div>
 			<div class="modal-footer">
 				<button type="button" class="btn btn-default" data-dismiss="modal">閉じる</button>
