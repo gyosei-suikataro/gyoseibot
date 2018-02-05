8d89012722c6f7d14374c0c7c68a267b86c8eb38
tomoaki nagasaka <tomoaki_nagasaka@gyosei-system.co.jp>
2018-02-05 15:12:20 +0900

    更新

--- web/shisetsu.php
+++ web/shisetsu.php
@@ -45,8 +45,9 @@ if ($link) {
                <th data-column-id='genre2'  data-width='10%'>ジャンル２</th>
                <th data-column-id='lat'  data-width='5%'>緯度</th>
                <th data-column-id='lng'  data-width='5%'>経度</th>
-               <th data-column-id='iurl'  data-width='20%'>画像URL</th>
-               <th data-column-id='url'  data-width='20%'>詳細URL</th>
+               <th data-column-id='iurl'  data-width='17%'>画像URL</th>
+               <th data-column-id='url'  data-width='17%'>詳細URL</th>
+               <th data-column-id='mod'  data-width='6%' data-formatter='details' data-sortable='false'></th>
            </tr>";
 	echo "</thead>";
 	echo "<tbody>";
@@ -97,8 +98,81 @@ if ($link) {
 <div class="container" align="center">
 	<input id="btn_del" type="button" class="btn btn-default" value="選択行の削除" onclick="drow()">
 	<input id="btn_ins" type="button" class="btn btn-default" value="施設の追加" onclick="irow()">
-	<input id="btn_mod" type="button" class="btn btn-default" value="施設の修正" onclick="mrow()">
+	<input id="btn_modal" type="button" style="display:none" data-toggle="modal"  data-target="#shosaiDialog" value="モーダル表示" />
 </div>
+</div>
+<div class="modal" id="shosaiDialog"  tabindex="-1">
+	<div class="modal-dialog">
+		<div class="modal-content" style="width:740px; margin-left: -20px;">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal">
+					<span aria-hidden="true">&times;</span>
+				</button>
+				<h4 class="modal-title" id="modal-label">施設登録</h4>
+			</div>
+			<div class="modal-body">
+				<form class="form-horizontal">
+					<div class="form-group">
+						<label class="col-sm-2 control-label" for="dia_meisho">施設名称</label>
+						<div class="col-sm-10">
+							<input id="dia_meisho" class="form-control" maxlength="40" placeholder="行政公園">
+						</div>
+					</div>
+					<div class="form-group">
+						<label class="col-sm-2 control-label" for="dia_jusho">住所</label>
+						<div class="col-sm-10">
+							<input id="dia_jusho" class="form-control" maxlength="128" placeholder="行政市行政1-1-1">
+						</div>
+					</div>
+					<div class="form-group">
+						<label class="col-sm-2 control-label" for="dia_tel">電話番号</label>
+						<div class="col-sm-10">
+							<input id="dia_tel" class="form-control" type="tel" maxlength="14" placeholder="000-000-0000">
+						</div>
+					</div>
+					<div class="form-group">
+						<label class="col-sm-2 control-label" for="dia_j1">ジャンル１</label>
+						<div class="col-sm-10">
+							<select class="form-control" id="dia_j1"  onChange="j1change()">
+						</div>
+					</div>
+					<div class="form-group">
+						<label class="col-sm-2 control-label" for="dia_j2">ジャンル２</label>
+						<div class="col-sm-10">
+							<select class="form-control" id="dia_j2" >
+						</div>
+					</div>
+					<div class="form-group">
+						<label class="col-sm-2 control-label" for="dia_latlng">緯度・経度</label>
+						<div class="col-sm-10">
+							<input id="dia_latlng" class="form-control" maxlength="33" placeholder="999.99999,999.99999">
+							<input type="button" class="btn btn-default" style="display:inline;" onclick="map()" value="地図の確認" style="width: 100px;"/>
+						</div>
+					</div>
+					<div class="form-group">
+						<label class="col-sm-2 control-label" for="dia_iurl">画像ＵＲＬ</label>
+						<div class="col-sm-10">
+							<input id="dia_iurl" class="form-control" maxlength="300" placeholder="https://www.yyy.zzz.jpg">
+							<input type="button" class="btn btn-default" style="display:inline;" onclick="image()" value="画像の確認" style="width: 100px;"/>
+							※必ずhttpsから始まるURLを指定してください
+						</div>
+					</div>
+					<div class="form-group">
+						<label class="col-sm-2 control-label" for="dia_iurl">詳細ＵＲＬ</label>
+						<div class="col-sm-10">
+							<input id="dia_url" class="form-control" maxlength="300" placeholder="http://www.yyy.zzz.html">
+							<input type="button" class="btn btn-default" style="display:inline;" onclick="image()" value="画像の確認" style="width: 100px;"/>
+							※必ずhttpsから始まるURLを指定してください
+						</div>
+					</div>
+				</form>
+			</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-default" >更新</button>
+				<button type="button" class="btn btn-default" data-dismiss="modal">閉じる</button>
+			</div>
+		</div>
+	</div>
 </div>
 <script>
 var rowIds = [];
@@ -112,8 +186,12 @@ $(function() {
 	$("#grid-basic").bootgrid({
 		selection: true,
 		multiSelect: true,
-		rowSelect: true,
 	    keepSelection: true,
+	    formatters: {
+	        "mod": function($column, $row) {
+	        	return "<input type='button' class='btn btn-default' value='修正' onclick='modwin("  + $row.id + ")' > ";
+             }
+	    }
 	}).on("selected.rs.jquery.bootgrid", function(e, rows)
 	{
 	    for (var i = 0; i < rows.length; i++)
@@ -168,6 +246,10 @@ function drow() {
 			alert("削除できませんでした");
 		}
 	}
+}
+
+function modwin(){
+	document.getElementById("btn_modal").click();
 }
 
 function irow(){
